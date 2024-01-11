3d2897a3f0dab5b7826660f983fdd088df4eae41
Fernando Vega <sonic497@hotmail.com>
2024-01-10 20:44:14 -0600

    Add files via upload

---
+++ booksData.py
@@ -0,0 +1,16 @@
+genres = ['thriller', 'novel', 'classic', 'self help', 'technical', 'sciences', 'financial', 'romance', 'psychology', 'philosophy', 'comedy',
+           'comics', 'manga', 'languages']
+
+books_info = [
+    ['self help', "The Subtle Art of not give a F*ck", '5', '2013', "Mark Manso"],
+    ['thriller', "The Psychoanalyst", '4', '2002', "John Katzenbach"],
+    ['technical', "Data Science for Dummies", '4', '2013', "Larry Holmes"],
+    ['financial', "I Will teach you to be rich", '4', '2009', "Ramit Sethi"],
+    ['sciences', "Astrophysics for people in a hurry", '5', '2017', "Neil deGrasse Tyson"],
+    ['classic', "Les misérables", '5', '1862', "Victor Hugo"],
+    ['psychology', "Influence: The Psychology of Persuasion", '4', '2006', "Robert B. Cialdini"],
+    ['romance', "Hello Beautiful", '3', '2022', "Ann Napolitano"],
+    ['comics', "The amazing Spider Man", '5', '1978', "Stan Lee"],
+    ['manga', "BRZRKR", '3', '2022', "Keanu Reeves"]
+    
+]
